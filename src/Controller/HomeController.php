275a0c9b2575ab620a2b2591cafca866414af2c3
MrHonda <>
2020-03-29 22:07:12 +0200

    Templates

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,15 +7,17 @@ namespace Percas\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("")
+     * @return Response
      */
-    public function index()
+    public function index(): Response
     {
-        return $this->json(['home']);
+        return $this->render('home.html.twig');
     }
 }
